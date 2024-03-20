94a5cd050c0ef5b9ac2eee4276146a09ed601d07
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2024-03-21 02:31:59 +0530

    Footer + Custom cursor

--- js/root.js
+++ js/root.js
@@ -13,4 +13,33 @@ $(document).ready(function () {
   clearInterval(loadingTimeout);
   $(".preloader").hide();
   onReady();
+
+  var downFor;
+
+  $(document)
+    .on("mousemove", function (e) {
+      $(".cursor").css({
+        left: e.clientX,
+        top: e.clientY,
+      });
+    })
+    .on("mousedown", function () {
+      $(".cursor").addClass("click");
+      downFor = new Date();
+    })
+    .on("mouseup", function () {
+      if (new Date() - downFor < 200) {
+        setTimeout(() => {
+          $(".cursor").removeClass("click");
+        }, 200);
+      } else {
+        $(".cursor").removeClass("click");
+      }
+    })
+    .on("mouseenter", "a", function () {
+      $(".cursor").addClass("hover");
+    })
+    .on("mouseleave", "a", function () {
+      $(".cursor").removeClass("hover");
+    });
 });
