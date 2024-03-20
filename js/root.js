06b3e67d86d292a059028cac02efcbf21d94d98a
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2024-03-21 03:02:00 +0530

    Cursor fixes + Z index

--- js/root.js
+++ js/root.js
@@ -12,37 +12,46 @@ var loadingTimeout = setInterval(() => {
 $(document).ready(function () {
   clearInterval(loadingTimeout);
   $(".preloader").hide();
+  $(".cursor").hide();
   onReady();
 
   var downFor;
 
-  $(document)
-    .on("mousemove", function (e) {
-      $(".cursor").css({
-        left: e.clientX,
-        top: e.clientY,
-      });
-    })
-    .on("mousedown", function (event) {
-      if (event.which === 3) {
-        return;
-      }
-      $(".cursor").addClass("click");
-      downFor = new Date();
-    })
-    .on("mouseup", function () {
-      if (new Date() - downFor < 200) {
-        setTimeout(() => {
+  if (window.innerWidth > 768) {
+    $(document)
+      .on("mouseenter", function () {
+        $(".cursor").show();
+      })
+      .on("mouseleave", function () {
+        $(".cursor").hide();
+      })
+      .on("mousemove", function (e) {
+        $(".cursor").css({
+          left: e.clientX,
+          top: e.clientY,
+        });
+      })
+      .on("mousedown", function (event) {
+        if (event.which === 3) {
+          return;
+        }
+        $(".cursor").addClass("click");
+        downFor = new Date();
+      })
+      .on("mouseup", function () {
+        if (new Date() - downFor < 200) {
+          setTimeout(() => {
+            $(".cursor").removeClass("click");
+          }, 200);
+        } else {
           $(".cursor").removeClass("click");
-        }, 200);
-      } else {
-        $(".cursor").removeClass("click");
-      }
-    })
-    .on("mouseenter", "a", function () {
-      $(".cursor").addClass("hover");
-    })
-    .on("mouseleave", "a", function () {
-      $(".cursor").removeClass("hover");
-    });
+        }
+      })
+      .on("mouseenter", "a", function () {
+        $(".cursor").addClass("hover");
+      })
+      .on("mouseleave", "a", function () {
+        $(".cursor").removeClass("hover");
+      });
+  }
 });
