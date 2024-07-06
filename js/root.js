b36251e78ab5e4c2dac2fe1864aebd6b680988eb
Arnav Gupta <cashlycash.g@gmail.com>
2024-07-06 16:11:01 +0530

    more edits

--- js/root.js
+++ js/root.js
@@ -10,48 +10,63 @@ var loadingTimeout = setInterval(() => {
 }, 500);
 
 $(document).ready(function () {
+  $(".navbar .container::after").on("click", function () {
+    $(".mobile").show();
+  });
+
   clearInterval(loadingTimeout);
   $(".preloader").hide();
-  $(".cursor").hide();
+  // $(".cursor").hide();
   onReady();
 
   var downFor;
 
-  if (window.innerWidth > 768) {
-    $(document)
-      .on("mouseenter", function () {
-        $(".cursor").show();
-      })
-      .on("mouseleave", function () {
-        $(".cursor").hide();
-      })
-      .on("mousemove", function (e) {
-        $(".cursor").css({
-          left: e.clientX,
-          top: e.clientY,
-        });
-      })
-      .on("mousedown", function (event) {
-        if (event.which === 3) {
-          return;
-        }
-        $(".cursor").addClass("click");
-        downFor = new Date();
-      })
-      .on("mouseup", function () {
-        if (new Date() - downFor < 200) {
-          setTimeout(() => {
-            $(".cursor").removeClass("click");
-          }, 200);
-        } else {
-          $(".cursor").removeClass("click");
-        }
-      })
-      .on("mouseenter", "a", function () {
-        $(".cursor").addClass("hover");
-      })
-      .on("mouseleave", "a", function () {
-        $(".cursor").removeClass("hover");
-      });
-  }
+  // if (window.innerWidth > 768) {
+  //   var mouseX = 0;
+  //   var mouseY = 0;
+
+  //   var mouseMove = function () {
+  //     $(".cursor").css({
+  //       left: mouseX,
+  //       top: mouseY,
+  //     });
+  //     window.requestAnimationFrame(mouseMove);
+  //   };
+
+  //   mouseMove();
+
+  //   $(document)
+  //     .on("mouseenter", function () {
+  //       $(".cursor").show();
+  //     })
+  //     .on("mouseleave", function () {
+  //       $(".cursor").hide();
+  //     })
+  //     .on("mousemove", function (e) {
+  //       mouseX = e.clientX;
+  //       mouseY = e.clientY;
+  //     })
+  //     .on("mousedown", function (event) {
+  //       if (event.which === 3) {
+  //         return;
+  //       }
+  //       $(".cursor").addClass("click");
+  //       downFor = new Date();
+  //     })
+  //     .on("mouseup", function () {
+  //       if (new Date() - downFor < 200) {
+  //         setTimeout(() => {
+  //           $(".cursor").removeClass("click");
+  //         }, 200);
+  //       } else {
+  //         $(".cursor").removeClass("click");
+  //       }
+  //     })
+  //     .on("mouseenter", "a", function () {
+  //       $(".cursor").addClass("hover");
+  //     })
+  //     .on("mouseleave", "a", function () {
+  //       $(".cursor").removeClass("hover");
+  //     });
+  // }
 });
