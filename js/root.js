4b2f715a226c1b9618e0f76fdd476689d352515a
Arnav Gupta <cashlycash.g@gmail.com>
2024-07-14 22:16:46 +0530

    manual delay for loader

--- js/root.js
+++ js/root.js
@@ -10,63 +10,65 @@ var loadingTimeout = setInterval(() => {
 }, 500);
 
 $(document).ready(function () {
-  $(".navbar .container::after").on("click", function () {
-    $(".mobile").show();
-  });
+  setTimeout(() => {
+    $(".navbar .container::after").on("click", function () {
+      $(".mobile").show();
+    });
 
-  clearInterval(loadingTimeout);
-  $(".preloader").hide();
-  // $(".cursor").hide();
-  onReady();
+    clearInterval(loadingTimeout);
+    $(".preloader").hide();
+    // $(".cursor").hide();
+    onReady();
 
-  var downFor;
+    var downFor;
 
-  // if (window.innerWidth > 768) {
-  //   var mouseX = 0;
-  //   var mouseY = 0;
+    // if (window.innerWidth > 768) {
+    //   var mouseX = 0;
+    //   var mouseY = 0;
 
-  //   var mouseMove = function () {
-  //     $(".cursor").css({
-  //       left: mouseX,
-  //       top: mouseY,
-  //     });
-  //     window.requestAnimationFrame(mouseMove);
-  //   };
+    //   var mouseMove = function () {
+    //     $(".cursor").css({
+    //       left: mouseX,
+    //       top: mouseY,
+    //     });
+    //     window.requestAnimationFrame(mouseMove);
+    //   };
 
-  //   mouseMove();
+    //   mouseMove();
 
-  //   $(document)
-  //     .on("mouseenter", function () {
-  //       $(".cursor").show();
-  //     })
-  //     .on("mouseleave", function () {
-  //       $(".cursor").hide();
-  //     })
-  //     .on("mousemove", function (e) {
-  //       mouseX = e.clientX;
-  //       mouseY = e.clientY;
-  //     })
-  //     .on("mousedown", function (event) {
-  //       if (event.which === 3) {
-  //         return;
-  //       }
-  //       $(".cursor").addClass("click");
-  //       downFor = new Date();
-  //     })
-  //     .on("mouseup", function () {
-  //       if (new Date() - downFor < 200) {
-  //         setTimeout(() => {
-  //           $(".cursor").removeClass("click");
-  //         }, 200);
-  //       } else {
-  //         $(".cursor").removeClass("click");
-  //       }
-  //     })
-  //     .on("mouseenter", "a", function () {
-  //       $(".cursor").addClass("hover");
-  //     })
-  //     .on("mouseleave", "a", function () {
-  //       $(".cursor").removeClass("hover");
-  //     });
-  // }
+    //   $(document)
+    //     .on("mouseenter", function () {
+    //       $(".cursor").show();
+    //     })
+    //     .on("mouseleave", function () {
+    //       $(".cursor").hide();
+    //     })
+    //     .on("mousemove", function (e) {
+    //       mouseX = e.clientX;
+    //       mouseY = e.clientY;
+    //     })
+    //     .on("mousedown", function (event) {
+    //       if (event.which === 3) {
+    //         return;
+    //       }
+    //       $(".cursor").addClass("click");
+    //       downFor = new Date();
+    //     })
+    //     .on("mouseup", function () {
+    //       if (new Date() - downFor < 200) {
+    //         setTimeout(() => {
+    //           $(".cursor").removeClass("click");
+    //         }, 200);
+    //       } else {
+    //         $(".cursor").removeClass("click");
+    //       }
+    //     })
+    //     .on("mouseenter", "a", function () {
+    //       $(".cursor").addClass("hover");
+    //     })
+    //     .on("mouseleave", "a", function () {
+    //       $(".cursor").removeClass("hover");
+    //     });
+    // }
+  }, 1500);
 });
